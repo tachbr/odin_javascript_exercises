87417165662adfb738e56cd5715dab564cbfd484
tachbr <nbrucher@t-online.de>
2021-06-12 17:11:45 +0200

    Solution

--- pigLatin/pigLatin.js
+++ pigLatin/pigLatin.js
@@ -1,5 +1,21 @@
-function pigLatin(string) {
+function pigLatin(testString) {
+
+  return testString.split(' ').map((word, index, arr) => arr[index] = checkWord(word)).join(' ');
 
 };
   
   module.exports = pigLatin;
+
+  function checkWord(word) {
+
+    let wordArray = [...word];
+    if ((/[^aeiou]/).test(wordArray[0])) {
+      let char = wordArray.shift();
+      if (char === 'q' && (/[u]/).test(wordArray[0])) char += wordArray.shift();
+      wordArray.push(char);
+      return checkWord(wordArray.join(''));
+    }
+    return wordArray.join('') + 'ay';
+  }
+
+
