b2bda3b35b465ce1aa3979aa280ae85fb11f5c33
tachbr <nbrucher@t-online.de>
2021-06-12 18:34:26 +0200

    Solution

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,6 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(testArray, ...args) {
+    args.forEach((arg) => {if (testArray.includes(arg)) testArray.splice(testArray.indexOf(arg), 1)});
+    return testArray;
 };
 
 module.exports = removeFromArray;
