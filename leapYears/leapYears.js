ff0fee93a5c30e59ef90da5c4dc1caabf6829eba
tachbr <nbrucher@t-online.de>
2021-06-12 20:21:47 +0200

    Solution

--- leapYears/leapYears.js
+++ leapYears/leapYears.js
@@ -1,5 +1,5 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    return (year % 100 === 0 && year % 400 === 0) ? true : (year % 4 === 0 && year %100 !== 0) ? true : false; 
 };
 
 module.exports = leapYears;
