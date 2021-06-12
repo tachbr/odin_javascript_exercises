09deb6cbc35f924b2238c181731645e76e91954c
tachbr <nbrucher@t-online.de>
2021-06-12 18:00:36 +0200

    Solution

--- findTheOldest/findTheOldest.js
+++ findTheOldest/findTheOldest.js
@@ -1,5 +1,10 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    return people.reduce((a, b) => (calcAge(a) > calcAge(b)) ? a : b);
 };
 
 module.exports = findTheOldest;
+
+function calcAge(a) {
+    if (!a.yearOfDeath) a.yearOfDeath = 2021;
+    return a.yearOfDeath - a.yearOfBirth;
+}
