03b80be2e34ed2917a8629d40708d0b338267429
tachbr <nbrucher@t-online.de>
2021-06-12 20:13:19 +0200

    Solution

--- snakeCase/snakeCase.js
+++ snakeCase/snakeCase.js
@@ -1,5 +1,7 @@
-const snakeCase = function() {
-
+const snakeCase = function(testString) {
+    if (testString.match(/[A-Z][a-z]/g) != null && testString.match(/[A-Z][a-z]/g).length > 1) testString = testString.toLowerCase();
+    return testString.replace(/\s|-|[.]{2}\b/g, '_').replace(/[!?'".,]/g, '').replace(/(?<=[a-z])[A-Z](?=[a-z])/, '_$&').toLowerCase();
 };
 
 module.exports = snakeCase;
+
