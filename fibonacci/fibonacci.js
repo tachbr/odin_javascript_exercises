e8a06bf93fd9ea38f398507d69f517156c75bbed
tachbr <nbrucher@t-online.de>
2021-06-12 20:42:24 +0200

    Solution

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,4 +1,6 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    if (num < 0) return 'OOPS';
+    return (num < 1) ? 0 : (num < 2) ? 1 : fibonacci(num - 1) + fibonacci(num - 2);
 
 };
 
