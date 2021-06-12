3e4cb7af1f81e679ed7cab5c0d5fa16662d53a84
tachbr <nbrucher@t-online.de>
2021-06-12 13:11:45 +0200

    Solution

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,7 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+    if (start < 0 || end < 0 || !Number.isInteger(start) || !Number.isInteger(end)) return 'ERROR';
+    
+    return (Math.max(end, start) - Math.min(end, start) + 1) * (end + start)/2;
 };
 
 module.exports = sumAll;
